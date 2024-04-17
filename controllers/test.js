e876e760e9009ba8b4f836cbb3db636c140e2074
Zan <thnhtr2000@gmail.com>
2024-04-17 17:43:48 +0700

    api

--- controllers/test.js
+++
@@ -1,4 +0,0 @@
-const sizes = ['XS', 'S', 'M', 'L', 'XL'];
-const sizesT = '["XS","X"]';
-console.log(' sizesT', JSON.parse(sizesT));
-console.log('typeof sizes', typeof JSON.parse(sizesT));
--- helpers/removeDuplicate.js
+++
@@ -1,3 +0,0 @@
-export const convertStringToObject = (string) => {
-    return JSON.parse(string);
-};
